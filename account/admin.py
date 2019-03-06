eb6c99704a479e51bb5f4db93e18a2d56b6f5d83
magicmangowagon <43754994+magicmangowagon@users.noreply.github.com>
2019-03-06 17:19:49 -0500

    added email field to profile

--- account/admin.py
+++ account/admin.py
@@ -4,4 +4,4 @@ from .models import Profile
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'date_of_birth', 'photo', 'role']
+    list_display = ['user', 'email', 'date_of_birth', 'photo', 'role']
