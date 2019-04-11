70fa344a66e7ab0fe23c84123827439ffe1556df
magicmangowagon <43754994+magicmangowagon@users.noreply.github.com>
2019-04-11 17:55:33 -0400

    Fixed issue with TC's having comps marked complete with unattempted LO's

--- rubrics/functions.py
+++ rubrics/functions.py
@@ -1,4 +1,4 @@
-from .models import RubricLine, Competency, CompetencyProgress
+from .models import RubricLine, Competency, CompetencyProgress, LearningObjective
 
 
 # processes rubriclines from view, checks if ready conditions are met, records result, saves the
@@ -19,8 +19,8 @@ def process_rubricLine(rubricLines):
 # if not, creates them and saves to DB with rubric lines tied to it.
 def assess_competency_done(rubricLines):
     competencies = Competency.objects.all()
-
     user = rubricLines[0].student.userOwner
+
     for competencies in competencies:
         if CompetencyProgress.objects.filter(competency=competencies, student=user).exists():
             break
@@ -31,26 +31,43 @@ def assess_competency_done(rubricLines):
                 student=user
             )
             compProg.save()
+
     compProgs = CompetencyProgress.objects.all().filter(student=user)
 
+    # Once a switch to turn LO's on and off for certain challenges and users is implemented
+    # this query below needs to be filtered by user, user rubricLine.student to do it in the filter
+    learningObjs = LearningObjective.objects.all()
+
     for object in compProgs:
-        for RubricLine in rubricLines:
-            if RubricLine.learningObjective.compGroup == object.competency.compGroup \
-                    and RubricLine.learningObjective.compNumber == object.competency.compNumber \
-                    and RubricLine.student.userOwner == object.student:
+        for learningObj in learningObjs:
+            # If a rubricLine doesn't exist for the LO then it wasn't attempted and the Competency
+            # cannot be complete
+            if rubricLines.filter(learningObjective=learningObj.id).exists() is False:
+                object.complete = False
+            else:
+                # User has attempted all LO's in the competency, proceed to check each one for
+                # completion
+                for RubricLine in rubricLines:
 
-                compProg = type(object).objects.get(pk=object.id)
-                rbID = type(RubricLine).objects.get(pk=RubricLine.id)
-                compProg.rubricLines.add(rbID)
-                compProg.save()
+                    # checks for existing rubricLines
+                    if RubricLine.learningObjective.compGroup == object.competency.compGroup \
+                            and RubricLine.learningObjective.compNumber == object.competency.compNumber \
+                            and RubricLine.student.userOwner == object.student:
 
-                object.attempted = True
-                if RubricLine.ready is False:
-                    object.complete = False
-                    break
-                else:
-                    object.complete = True
-                object.save()
-    print(compProgs.count())
+                        # get compProg instance and add a rubricLine relationship to it
+                        compProg = type(object).objects.get(pk=object.id)
+                        rbID = type(RubricLine).objects.get(pk=RubricLine.id)
+                        compProg.rubricLines.add(rbID)
+                        compProg.save()
+
+                        object.attempted = True
+                        if RubricLine.ready is False:
+                            object.complete = False
+                            break
+                        else:
+                            object.complete = True
+
+        object.save()
+
     return compProgs
 
