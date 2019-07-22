824d5d899ae0c3b5a060609bd571af8d38e182ba
magicmangowagon <43754994+magicmangowagon@users.noreply.github.com>
2019-07-22 12:07:46 -0400

    made new users TC's by default

--- account/models.py
+++ account/models.py
@@ -20,7 +20,7 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
     photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
-    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, null=True, blank=True)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, null=True, blank=True, default=1)
 
     def __str__(self):
         return 'Profile for user {}' .format(self.user.username)
