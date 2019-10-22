041ecb9e270ad6f30746804c5aa5bb7bc5545eab
magicmangowagon <43754994+magicmangowagon@users.noreply.github.com>
2019-10-22 15:55:58 -0400

    Added name of solution instance to list view for evaluators and coaches, added a sticky header to eval page to keep evaluate link and user info in view.

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e7+_=2dkn(4n64id2m_8v&fqvbw61f5u06mgp#io2ilwz$$drg'
-# SECRET_KEY = os.environ['SECRET_KEY']
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
