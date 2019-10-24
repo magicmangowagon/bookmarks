52a1406525bbec360f9858f5c1b63163266dfd78
magicmangowagon <43754994+magicmangowagon@users.noreply.github.com>
2019-10-24 14:03:27 -0400

    File storage working

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -151,7 +151,8 @@ STATIC_URL = 'https://' + AWS_S3_CUSTOM_DOMAIN + AWS_LOCATION
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL)
-
+MEDIA_ROOT = os.path.join(STATIC_URL, '/media')
+DEFAULT_FILE_STORAGE = STATICFILES_STORAGE
 
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
