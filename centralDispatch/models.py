20e12b903f541195c2f9cc181e8bc0ed1a51ef21
magicmangowagon <43754994+magicmangowagon@users.noreply.github.com>
2020-05-28 12:53:32 -0400

    Added info section

--- centralDispatch/models.py
+++ centralDispatch/models.py
@@ -47,7 +47,7 @@ def create_solution_router(sender, **kwargs):
 # Generated on Solution submission. Reviews are manually assigned by admin at the moment
 class AssignmentKeeper(models.Model):
     evaluator = models.ForeignKey(Profile, null=True, on_delete=models.PROTECT, related_name='evaluator', blank=True,
-                                  limit_choices_to=Q(role=2) | Q(role=3))
+                                  limit_choices_to=Q(role=2) | Q(role=3) | Q(role=4))
     coach = models.ForeignKey(Profile, null=True, on_delete=models.PROTECT, related_name='coach', blank=True,
                               limit_choices_to=Q(role=2) | Q(role=3))
     userSolution = models.ForeignKey(UserSolution, blank=True, default='', on_delete=models.CASCADE)
